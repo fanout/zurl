92a151f85958891d927dfcd8b0e6746cc17f8a58
Justin Karneges <justin@affinix.com>
2013-11-24 13:26:49 -0800

    be more explicit to work around warning

--- package.sh
+++ package.sh
@@ -9,6 +9,6 @@ fi
 VERSION=$1
 
 mkdir -p build/zurl-$VERSION
-cp -a * .gitignore build/zurl-$VERSION
+cp -a .gitignore COPYING README configure zurl.qc zurl.pro zurl.conf.example qcm src tools tests build/zurl-$VERSION
 cd build
 tar jcvf zurl-$VERSION.tar.bz2 zurl-$VERSION
