b4b88b60e007376df3cfec9b996c91a372644e74
Justin Karneges <justin@affinix.com>
2014-09-15 09:25:25 -0700

    update package script

--- package.sh
+++ package.sh
@@ -10,6 +10,6 @@ VERSION=$1
 
 mkdir -p build/zurl-$VERSION
 cp -a .gitignore COPYING README.md configure zurl.qc zurl.pro zurl.conf.example qcm src tools tests build/zurl-$VERSION
-rm -rf build/zurl-$VERSION/src/qzmq/.git build/zurl-$VERSION/src/common/.git
+rm -rf build/zurl-$VERSION/src/qzmq/.git build/zurl-$VERSION/src/common/.git build/zurl-$VERSION/src/jdns/.git
 cd build
 tar jcvf zurl-$VERSION.tar.bz2 zurl-$VERSION
