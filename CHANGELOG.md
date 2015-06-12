378e41ce7b875cd078aac5e5f98f83522c1254ed
Justin Karneges <justin@affinix.com>
2015-06-11 18:33:09 -0700

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+Zurl Changelog
+==============
+
+v. 1.4.0 (2015-06-11)
+
+  * follow-redirects option.
+  * timeout option.
+  * id is now optional.
+  * Ack stream requests immediately so they become referenceable.
+  * Respond with headers even before initial body received.
+
+v. 1.3.1 (2014-09-16)
+
+v. 1.3.0 (2014-02-13)
+
+v. 1.1.0 (2013-11-24)
+
+v. 1.0.0 (2013-08-10)
+
+  * Stable version.
--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ fi
 VERSION=$1
 
 mkdir -p build/zurl-$VERSION
-cp -a .gitignore COPYING README.md configure zurl.qc zurl.pro zurl.conf.example qcm src tools tests build/zurl-$VERSION
+cp -a .gitignore CHANGELOG.md COPYING README.md configure zurl.qc zurl.pro zurl.conf.example qcm src tools tests build/zurl-$VERSION
 rm -rf build/zurl-$VERSION/src/qzmq/.git build/zurl-$VERSION/src/common/.git build/zurl-$VERSION/src/jdns/.git
 cd build
 tar jcvf zurl-$VERSION.tar.bz2 zurl-$VERSION
