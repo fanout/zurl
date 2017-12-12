dff8232856e17bf4fe6f0076c06fb9fbfdffcf18
Justin Karneges <justin@karneges.com>
2017-12-12 14:31:29 -0800

    add note about commercial license

--- src/websocket.h
+++ src/websocket.h
@@ -1,18 +1,29 @@
 /*
  * Copyright (C) 2014 Fanout, Inc.
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of Zurl.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * $FANOUT_BEGIN_LICENSE:GPL$
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Zurl is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Zurl is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Alternatively, this file may be used under the terms of a commercial Fanout
+ * license, where the commercial license agreement is provided with the
+ * software or contained in a written agreement between you and Fanout. For
+ * further information use the contact form at <https://fanout.io/enterprise/>.
+ *
+ * $FANOUT_END_LICENSE$
  */
 
 #ifndef WEBSOCKET_H
